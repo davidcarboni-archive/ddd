34a0772985082e44572eae5a1f3f175eda944361
David Carboni <david@carboni.io>
2018-03-23 19:23:13 +0000

    Changing to Go build from Go run.

---
+++ google/container/build/main.go
@@ -0,0 +1,25 @@
+// https://blog.codeship.com/building-minimal-docker-containers-for-go-applications/
+
+package main
+
+import (
+    "fmt"
+    "io/ioutil"
+    "net/http"
+    "os"
+)
+
+func main() {
+    resp, err := http.Get("https://google.com")
+    check(err)
+    body, err := ioutil.ReadAll(resp.Body)
+    check(err)
+    fmt.Printf("Length of content received from google.com: %d\n", len(body))
+}
+
+func check(err error) {
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+}
---
+++ google/container/build/readme.txt
@@ -0,0 +1,2 @@
+A Go build and executable run in a container.
+But it leaves the source file hanging around.
